b62d2ad635680eef4e677ac59a31ba929dfb6f3f
Lovepreet Singh <singhluv2003@gmail.com>
2023-02-21 21:10:01 -0800

    added Section about

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 function Header() {
     return(
         <div>
@@ -11,7 +10,7 @@ function Header() {
                         </a>      
                     </li>
                     <li class="header--list">
-                        <a href="#">
+                        <a href="#sectionAbout">
                             <span className="header--icon"><ion-icon name="information-circle-outline"></ion-icon></span>
                             <span className="header--text">About</span>
                         </a>      
@@ -23,7 +22,7 @@ function Header() {
                         </a>      
                     </li>
                     <li class="header--list">
-                        <a href="#">
+                        <a href="#footer">
                             <span className="header--icon"><ion-icon name="call-outline"></ion-icon></span>
                             <span className="header--text">Contact</span>
                         </a>      
@@ -37,7 +36,7 @@ function Header() {
 const Footer = () => {
     return(
         <div>
-            <footer className="footer--body">
+            <footer className="footer--body" id="footer">
                 <ul className="footer--links">
                     <li className="footer--team">
                         <span className="footer--icon"><ion-icon name="people-outline"></ion-icon></span>
@@ -62,10 +61,44 @@ const Footer = () => {
     );
 }
 
-const Section = () => {
+const SectionAbout = () => {
     return(
         <div>
-            <section className="section--body">
+            <section className="sectionAbout--body" id="sectionAbout">
+                <div className="sectionAbout--mainText">
+                    <h2>.about<span>Us</span></h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero perferendis a aliquid quia dolorem officiis enim dicta soluta temporibus ipsam, 
+                    accusamus laudantium nihil, eum commodi est iusto eaque sed repellendus. 
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est autem rerum enim odit, 
+                    non sint voluptate temporibus sed, cumque itaque tempore incidunt eum doloribus odio reiciendis, 
+                    maiores saepe corporis magni?</p>
+                </div>
+                <div className="sectionAbout--teamContent">
+                    <div className="sectionAbout--boxContent">
+                        <div className="sectionAbout--iconContent"><ion-icon name="people-circle-outline"></ion-icon></div>
+                        <h3>Team1</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non debitis iusto facilis dolores nisi 
+                        culpa reiciendis minus sapiente ad delectus, distinctio quas quod, totam suscipit? Porro id 
+                        maxime sint totam!</p>
+                        <a href="#" className="sectionAbout--links">See More</a>
+                    </div>
+                    <div className="sectionAbout--boxContent">
+                        <div className="sectionAbout--iconContent"><ion-icon name="people-circle-outline"></ion-icon></div>
+                        <h3>Team1</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non debitis iusto facilis dolores nisi 
+                        culpa reiciendis minus sapiente ad delectus, distinctio quas quod, totam suscipit? Porro id 
+                        maxime sint totam!</p>
+                        <a href="#" className="sectionAbout--links">See More</a>
+                    </div>
+                    <div className="sectionAbout--boxContent">
+                        <div className="sectionAbout--iconContent"><ion-icon name="people-circle-outline"></ion-icon></div>
+                        <h3>Team1</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non debitis iusto facilis dolores nisi 
+                        culpa reiciendis minus sapiente ad delectus, distinctio quas quod, totam suscipit? Porro id 
+                        maxime sint totam!</p>
+                        <a href="#" className="sectionAbout--links">See More</a>
+                    </div>
+                </div>
             </section>
         </div>
     );
@@ -78,7 +111,7 @@ class Container extends React.Component{
         return(
             <div>
                 <Header />
-                <Section />
+                <SectionAbout />
                 <Footer />
             </div>
         )
