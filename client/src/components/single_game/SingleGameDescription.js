d844e105ef38f41d49ade50f2562650ee51c44a8
J e r r y <alfandre121@gmail.com>
2023-03-25 09:30:08 -0700

    Comments Section Update

--- client/src/components/single_game/SingleGameDescription.js
+++ client/src/components/single_game/SingleGameDescription.js
@@ -1,7 +1,20 @@
 import "../../css_files/sectionArcade_style.css";
 import Poster from "../../test_data/poster.jpg";
+import {useState} from "react";
+
+import SingleGameComment from "./SingleGameComment";
 
 const SingleGameDescription = () => {
+
+  const [newComment, setNewComment] = useState("");
+
+  const handleCommentChange = (e) => {
+    let comment_aux = e.target.value;
+    if(comment_aux.length <= 255) {
+      setNewComment(comment_aux);
+    }
+  };
+
   return (
     <div className="SingleGameDescription_Wrapper">
       <div className="SingleGameTop_Wrapper">
@@ -32,26 +45,59 @@ const SingleGameDescription = () => {
             id="comment_input"
             rows="4"
             placeholder="Share your thoughts..."
+            value={newComment}
+            onChange={handleCommentChange}
           ></textarea>
           <div className="AddComment_BottomWrapper">
             <div className="AddComment_Sent">
               Sent
             </div>
             <div className="AddComment_WordCount">
-              <span>Max Words: 100 - </span>
-              <span> Word Count: </span>
+              <span>Max Char: 255 - </span>
+              <span> Char Count: <strong>{newComment.length}</strong></span>
             </div>
           </div>
         </div>
       </div>
       <div className="SingleGameComments_Wrapper">
-        <h3 className="SingleGame_SubTitle">Comments</h3>
-        <div className="CommentsSection_Container">
-          
+        <h3 className="SingleGame_SubTitle">Comments: <span>{comments_test.length}</span></h3>
+        <div className="AddComment_Container CommentsSection_Container">
+          {
+            comments_test.length === 0 ?
+              <div className="NoComments_Wrapper"> 
+                <h5>NO COMMENTS YET</h5>
+              </div>
+              :
+              comments_test.map((comment) => {
+                return <SingleGameComment key={comment.id} comment={comment.comment}
+                  author={comment.author} date={comment.date}/>
+              })
+          }
         </div>
       </div>
     </div>
   );
 };
+
+const comments_test = [
+ {
+  id: 1,
+  comment: "lorem ipsum",
+  author: "John Doe",
+  date: new Date(2022,3,1)
+ },
+ {
+  id: 2,
+  comment: "lorem ipsum",
+  author: "John Doe",
+  date: new Date(2022,3,1)
+ },
+ {
+  id: 3,
+  comment: "lorem ipsum",
+  author: "John Doe",
+  date: new Date(2022,3,1)
+ }
+]
 
 export default SingleGameDescription;
