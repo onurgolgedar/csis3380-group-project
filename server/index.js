37158323990de97d61b3298e57781e4cd4da201d
Onur Gölgedar <b.onur.golgedar@gmail.com>
2023-03-27 13:13:27 -0700

    dev: researching mongoose
    Mongoose is a Node.js-based Object Data Modeling (ODM) library for MongoDB.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,32 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+var axios = require("axios");
+var data = JSON.stringify({
+  collection: "games",
+  database: "DB1",
+  dataSource: "csis3380-group-project",
+});
+
+// Test api-key: 56nyq9QrPFlGBYivMRZ4GuYzKMn2kPm7MztaDbcQLLsKpIz2dSvQbWqhdSD6eYjI
+var config = {
+  method: "post",
+  url: "https://us-east-1.aws.data.mongodb-api.com/app/data-mfxen/endpoint/data/v1/action/findOne",
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Request-Headers": "*",
+    "api-key":
+      "56nyq9QrPFlGBYivMRZ4GuYzKMn2kPm7MztaDbcQLLsKpIz2dSvQbWqhdSD6eYjI",
+  },
+  data: data,
+};
+
+axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
