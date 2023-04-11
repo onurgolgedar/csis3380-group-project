c7f5e8de0823e60231fde11e07171ce10f027fa1
J e r r y <alfandre121@gmail.com>
2023-04-11 14:55:40 -0700

    Attempt #7

--- server/index.js
+++ server/index.js
@@ -35,7 +35,9 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    maxAge: 24 * 60 * 60 * 1000
+    maxAge: 24 * 60 * 60 * 1000,
+    sameSite:'none',
+    secure: true
   } 
 }));
 app.use(passport.initialize());
