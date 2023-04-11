72a58ad2f75a3823514c5801611e3debcb020bba
J e r r y <alfandre121@gmail.com>
2023-04-11 14:50:40 -0700

    Fix Attempt #6

--- server/index.js
+++ server/index.js
@@ -35,8 +35,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    maxAge: 24 * 60 * 60 * 1000,
-    secure: true
+    maxAge: 24 * 60 * 60 * 1000
   } 
 }));
 app.use(passport.initialize());
